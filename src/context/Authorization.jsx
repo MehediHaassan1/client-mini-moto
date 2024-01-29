67ca49b8111c5f8784cfb2825e4ffed55259d04d
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-29 17:34:55 +0600

    web access token got in clientSide

--- src/context/Authorization.jsx
+++ src/context/Authorization.jsx
@@ -51,8 +51,26 @@ const Authorization = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-            setLoading(false);
+            if (currentUser) {
+                setUser(currentUser);
+                setLoading(false);
+                const loggedUser = { email: currentUser?.email };
+                fetch(`http://localhost:5000/web-access-token`, {
+                    method: "POST",
+                    header: {
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify(loggedUser),
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const token = data.token;
+                        localStorage.setItem("mini-moto-web-token", token);
+                    });
+            } else {
+                setUser(null);
+                localStorage.removeItem("mini-moto-web-token");
+            }
         });
 
         return () => {
